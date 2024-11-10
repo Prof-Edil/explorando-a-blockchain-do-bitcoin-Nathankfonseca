9712e53d13e42a87682e35e70a245ed7a186755a
Nathankfonseca <nathankfonseca@hotmail.com>
2024-11-10 16:20:44 -0300

    003.sh

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
 # How many new outputs were created by block 123,456?
+bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_253 -rpcpassword=eRHxrWVSaWHw getblockheader 0000000000002917ed80650c6174aac8dfc46f5fe36480aaef682ff6cd83c3ca
