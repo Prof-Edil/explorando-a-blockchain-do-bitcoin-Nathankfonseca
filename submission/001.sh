f343c150440fa3c874f833eb47f6a4a77f811d70
Nathankfonseca <nathankfonseca@hotmail.com>
2024-11-10 15:53:15 -0300

    001.sh

--- submission/001.sh
+++ submission/001.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # What is the hash of block 654,321?
-./bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_253 -rpcpassword=eRHxrWVSaWHw getblockhash 654321
+bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_253 -rpcpassword=eRHxrWVSaWHw getblockhash 654321
