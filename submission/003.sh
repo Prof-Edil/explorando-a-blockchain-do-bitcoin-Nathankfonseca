d06ea1c26946773a4536e0763e58fe6f3b273aed
Nathankfonseca <nathankfonseca@hotmail.com>
2024-11-20 16:30:49 -0300

    Update 003.sh

--- submission/003.sh
+++ submission/003.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # How many new outputs were created by block 123,456?
 bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_253 -rpcpassword=eRHxrWVSaWHw getblockstats 123456 '["outs"]'
+
