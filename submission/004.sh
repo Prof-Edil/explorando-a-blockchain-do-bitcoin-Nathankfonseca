1ccd191b7fa00af0e6a449729e6769958ebfaf67
Nathankfonseca <nathankfonseca@hotmail.com>
2024-11-20 16:23:53 -0300

    004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -1,3 +1,3 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
-/bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_253 -rpcpassword=eRHxrWVSaWHw deriveaddresses "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2)#t7hec7kv"
+bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_253 -rpcpassword=eRHxrWVSaWHw deriveaddresses "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2)#t7hec7kv"
