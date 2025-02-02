4fe5455245782fe3f3fa316a6af82b698cd01f8b
Nathankfonseca <nathankfonseca@hotmail.com>
2025-02-02 16:48:06 -0300

    Update 005.sh

--- submission/005.sh
+++ submission/005.sh
@@ -1,7 +1,7 @@
 # Create a 1-of-4 P2SH multisig address from the public keys in the four inputs of this tx:
 #   `37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517`
-#!/bin/bash
 
+#!/bin/bash
 transacao=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_253 -rpcp>
 
 for i in $(seq 0 3);
