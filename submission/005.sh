c0ce041b992e58fefa4f4cca86b1f0d7cd9a910f
Nathankfonseca <nathankfonseca@hotmail.com>
2025-02-02 17:24:14 -0300

    Update 005.sh

--- submission/005.sh
+++ submission/005.sh
@@ -5,12 +5,12 @@
 
 transacao=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_253 -rpcpassword=eRHxrWVSaWHw getrawtransaction 37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517 1)
 
-for i in $(seq 0 3);
-do
-pub[$i]=$(echo "$transacao" | jq -r ".vin[$i].txinwitness[1]")
-done
+pub1=$(echo "$transacao" | jq -r ".vin[0].txinwitness[1]")
+pub2=$(echo "$transacao" | jq -r ".vin[1].txinwitness[1]")
+pub3=$(echo "$transacao" | jq -r ".vin[2].txinwitness[1]")
+pub4=$(echo "$transacao" | jq -r ".vin[3].txinwitness[1]")
 
-multi=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_253 -rpcpassword=eRHxrWVSaWHw createmultisig 1 "[\"$pub[0]\",\"$pub[1]\",\"$pub3[2]\",\"$pub4[3]\"]")
+multi=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_253 -rpcpassword=eRHxrWVSaWHw createmultisig 1 "[\"$pub1\",\"$pub2\",\"$pub3\",\"$pub4\"]")
 
 echo $multi | jq -r ".address"
 
