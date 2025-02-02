dfb6dcaeb0cf464c11a60d784e5d09206a2f54f3
Nathankfonseca <nathankfonseca@hotmail.com>
2025-02-02 17:19:42 -0300

    Update 005.sh

--- submission/005.sh
+++ submission/005.sh
@@ -2,14 +2,15 @@
 #   `37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517`
 
 #!/bin/bash
+
 transacao=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_253 -rpcpassword=eRHxrWVSaWHw getrawtransaction 37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517 1)
 
 for i in $(seq 0 3);
 do
-pub1=$(echo "$transacao" | jq -r ".vin[$i].txinwitness[1]")
+pub[$i]=$(echo "$transacao" | jq -r ".vin[$i].txinwitness[1]")
 done
 
-multi=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_253 -rpcpassword=eRHxrWVSaWHw createmultisig 1 "[\"$pub1\",\"$pub2\",\"$pub3\",\"$pub4\"]")
+multi=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_253 -rpcpassword=eRHxrWVSaWHw createmultisig 1 "[\"$pub[0]\",\"$pub[1]\",\"$pub3[2]\",\"$pub4[3]\"]")
 
 echo $multi | jq -r ".address"
 
